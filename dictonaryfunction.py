7b9499c094ea19f0139db3836e2c8124109749da
sushantd492-ai <sushantd492@gmail.com>
2026-06-17 11:41:52 +0530

    Add files via upload

---
+++ dictonaryfunction.py
@@ -0,0 +1,28 @@
+
+d1=dict(a='one',b='two')
+print(d1)
+print(d1.keys())
+print(d1.values())
+print(d1.items())
+print(d1.get('a'))
+print(d1.popitem())
+print(d1)
+print(d1.pop('a'))
+print(d1)
+d1['a']="two"
+print(d1)
+d1['a']="one"
+print(d1)
+d1.clear()
+print(d1)
+
+
+
+
+s='''python is very easy python is programming language'''
+words=s.lower().split()
+d=()
+l=
+for i in l :
+    d[i]=l.count(i)
+    print(d)
