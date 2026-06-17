a01c0aab59cb12557a158ac8eaf6e9e4d65450ea
sushantd492-ai <sushantd492@gmail.com>
2026-06-17 11:40:48 +0530

    Add files via upload

---
+++ loopingstatement.py
@@ -0,0 +1,26 @@
+if(10/2==0):
+  print("number is ever")
+
+
+#wap to check number enter number is multiple of 5 or noto
+
+a=input("enter your number")
+if a%5==0 :
+print(a)
+
+
+
+#wap to check the number is positive or not
+ a=input("enter your number")
+if a>0:
+print("number is positive")
+
+
+
+
+
+#wap to check given shape is rectangle or square
+if a==b:
+ print("it is square")
+else:
+ print("it is rectangle")
